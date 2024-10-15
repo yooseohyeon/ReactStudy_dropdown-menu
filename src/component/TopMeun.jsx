f854f33490b137663c90e46c27d57b5592218a22
yooseohyeon <yooseohyeon.02@gmail.com>
2024-10-15 15:55:50 +0900

    refactor: SVG transform 변경 및 <g> 태그 제거

--- src/component/TopMeun.jsx
+++ src/component/TopMeun.jsx
@@ -14,10 +14,8 @@ export default function TopMeun() {
       <div className="meun-header">
         <h3>고객센터</h3>
         <button onClick={viewDropdown}>
-          <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368">
-            <g transform={isOpen ? "rotate(180, 480, -480)" : undefined}>
-              <path d="M480-344 240-584l56-56 184 184 184-184 56 56-240 240Z"/>
-            </g>
+          <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368" transform={isOpen ? "rotate(180)" : undefined}>
+            <path d="M480-344 240-584l56-56 184 184 184-184 56 56-240 240Z"/>
           </svg>
         </button>
       </div>
